d6b3f57cc68429e0fad482f6830f7740902dcdf4
peterrsamir <peter.samir399@gmail.com>
2025-06-04 23:20:12 +0300

    Remove Main Storyboard and add new SplashViewController

--- CineNest/SplashViewController.swift
+++ CineNest/SplashViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SplashViewController.swift
 //  CineNest
 //
 //  Created by Peter on 04/06/2025.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SplashViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
 }
-
